3f8163a6f29cc5bff3f046d5ad671c98b645f0d7
Kathy Chen <kathychen.social@gmail.com>
2019-04-19 00:32:27 -0400

    DB setup

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+-- Create the database wishes_db and specified it for use.
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+-- Create the table wishes.
+CREATE TABLE burgers
+(
+    id int NOT NULL
+    AUTO_INCREMENT,
+burger_name varchar
+    (255) NOT NULL,
+devoured BOOLEAN,
+PRIMARY KEY
+    (id)
+);
---
+++ db/seed.sql
@@ -0,0 +1,18 @@
+INSERT INTO burgers
+    (
+    burger_name,
+    devoured
+    )
+VALUES
+    (
+        "Beef Double Cheese Burger",
+        0
+),
+    (
+        "Spicy Chicken Burger",
+        0
+),
+    (
+        "Big Mac",
+        0
+);
