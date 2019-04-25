461f9b82fb4da3c15229bdcad0059dffe183aa2f
Kathy Chen <kathychen.social@gmail.com>
2019-04-24 23:49:46 -0400

    progress on DB

--- db/seed.sql
+++ db/seed.sql
@@ -1,18 +1,14 @@
-INSERT INTO burgers
-    (
+INSERT INTO burgers (
     burger_name,
     devoured
-    )
-VALUES
-    (
-        "Cheese Burger",
-        0
-),
-    (
-        "Spicy Chicken Burger",
-        0
-),
-    (
-        "Big Mac",
-        0
+)
+
+VALUES (
+	"Cheese Burger", 0
+), (
+	"Spicy Chicken Burger", 0
+), (
+	"Big Mac", 0
 );
+
+SELECT * FROM burgers;
