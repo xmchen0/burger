d51013892ea3cab0de66444af74813a2638ae46d
Kathy Chen <kathychen.social@gmail.com>
2019-04-26 20:49:06 -0400

    experiencing connection lost server closed the connection error

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,10 @@
--- Create the database wishes_db and specified it for use.
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
--- Create the table wishes.
 CREATE TABLE burgers (
-    id int (10) NOT NULL
-    AUTO_INCREMENT,
-burger_name varchar
-    (255) NOT NULL,
-devoured BOOLEAN,
-PRIMARY KEY
-    (id)
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN,
+	PRIMARY KEY (id)
 );
-
-SELECT * FROM burgers;
