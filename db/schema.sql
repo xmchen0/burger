98f673511d1c38ca020b514bd6d127dcf683f28f
Kathy Chen <kathychen.social@gmail.com>
2019-04-27 23:52:10 -0400

    added burger menu

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,13 @@
--- DROP database IF EXISTS burgers_db; // Activate to refresh database only
+-- DROP database IF EXISTS burgers_db; // to refresh database
 CREATE DATABASE burgers_db;
 USE burgers_db;
+
+CREATE TABLE menu (
+	id int NOT NULL AUTO_INCREMENT,
+	menuName varchar(255) NOT NULL,
+-- 	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
 
 CREATE TABLE burgers (
 	id int NOT NULL AUTO_INCREMENT,
